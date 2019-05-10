192e77a92589b70584e2f2cad1d0594cfd9bb79a
VSCG <39382021+VSCG@users.noreply.github.com>
2019-05-10 18:30:49 +0530

    updated

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import { Button, FormGroup, FormControl } from "react-bootstrap";
 import "../../styles/login/Login.scss";
 
@@ -7,74 +7,80 @@ export default class Login extends Component {
 	super(props);
 
 	this.state = {
-	  selectedOption: "Sign In",
 	  username: "",
 	  email: "",
 	  password: "",
 	  keepSiginedIn: true,
-	  userType: "consumer"
+	  register: false,
+	  userType: 0
 	};
   }
 
   handleChange = event => {
 	  if(event.target.type === 'checkbox')
-	  {
 		this.setState({[event.target.id]: !this.state[event.target.id]});
-	  }
 	  else
-	  {
 		this.setState({[event.target.id]: event.target.value});
-	  }
   }
-
   handleOptionChange = changeEvent => {
-  	this.setState({
-			selectedOption: changeEvent.target.value
-  		}
-  	);
+	  this.setState({register: JSON.parse(changeEvent.target.value)});
   }
-
-  handleTypeChange = changeEvent => {this.setState({userType: changeEvent.target.value});}
+  handleTypeChange = changeEvent => {
+	  this.setState({userType: JSON.parse(changeEvent.target.value)});
+  }
   
   handleSubmit = event => {
-	event.preventDefault();
+	  event.preventDefault();
+  }
+  handleForgotPassword = event => {
+	  event.preventDefault();
   }
 
   render() {
+	const hideClass = this.state.register ? "hide-sign-in" : "hide-sign-up";
+	const btnName = this.state.register ? "Register" : "Login";
 	return (
 	<div className="login-wrap">
 		<div className="login">
-			<input id="tab-1" type="radio" name="tab" className="sign-in" value="Sign In" checked={this.state.selectedOption === "Sign In"} onChange={this.handleOptionChange}/><label htmlFor="tab-1" className="tab">Sign In</label>
-			<input id="tab-2" type="radio" name="tab" className="sign-up" value="Sign Up" checked={this.state.selectedOption === "Sign Up"} onChange={this.handleOptionChange}/><label htmlFor="tab-2" className="tab">Sign Up</label>
+			<div className="img-wrap">	
+				<img className="logo" src={ require('../../img/logo.png') } />
+			</div>
 			<div className="login-form">
-				<div className="sign-in-form">
-					<form role="form" onSubmit={this.handleSubmit}>
-						<div className="sign-in-wrap">
-							<FormGroup controlId="username">
-								<FormControl required autoFocus type="text" placeholder="Username" value={this.state.username} onChange={this.handleChange}/>
-							</FormGroup>
-							<FormGroup controlId="email" className="sign-up-fields">
-								<FormControl required autoFocus type="email" placeholder="Email" value={this.state.email} onChange={this.handleChange}/>
-							</FormGroup>
-							<FormGroup controlId="password">
-								<FormControl required type="password" placeholder="Password" 
-									pattern = "(?=^.{6,}$).*$" title = "Min 6 characters"
-									value={this.state.password} onChange={this.handleChange}/>
-							</FormGroup>
-							<FormGroup controlId="keepSiginedIn" className="checkbox-wrap sign-in-fields">
-								<FormControl autoFocus type="checkbox" checked={this.state.keepSiginedIn} onChange={this.handleChange}/>
-								<label htmlFor="keepSiginedIn" className="checkbox-label"><span>Keep me Signed in</span></label>
-							</FormGroup>
-							<div className="checkbox-wrap sign-up-fields">
-								<input id="consumer" type="radio" name="usertype" className="consumer" value="Consumer" checked={this.state.userType === "consumer"} onChange={this.handleTypeChange}/>
-								<label htmlFor="consumer" className="checkbox-label">User</label>
-								<input id="agent" type="radio" name="usertype" className="consumer" value="Agent" checked={this.state.userType === "Agent"} onChange={this.handleTypeChange}/>
-								<label htmlFor="agent" className="checkbox-label">Admin</label>
-							</div>
+				<form onSubmit={this.handleSubmit}>
+					<div className="fields-wrap">
+						<FormGroup>
+							<input id="username" required autoFocus type="text" placeholder="Username" value={this.state.username} onChange={this.handleChange}/>
+						</FormGroup>
+						<FormGroup className={"sign-up-fields " + hideClass}>
+							<input id="email" required autoFocus type="email" placeholder="Email" value={this.state.email} onChange={this.handleChange}/>
+						</FormGroup>
+						<FormGroup>
+							<input id="password" required type="password" placeholder="Password" pattern = "(?=^.{6,}$).*$" title = "Min 6 characters" value={this.state.password} onChange={this.handleChange}/>
+						</FormGroup>
+						<FormGroup className={"checkbox-wrap sign-in-fields " + hideClass}>
+							<input id="keepSiginedIn" autoFocus type="checkbox" checked={this.state.keepSiginedIn} onChange={this.handleChange}/>
+							<label htmlFor="keepSiginedIn" className="checkbox-label"><span>Keep me Signed in</span></label>
+						</FormGroup>
+						<FormGroup className={"checkbox-wrap sign-up-fields " + hideClass}>
+							<input id="consumer" type="radio" name="usertype" className="consumer" value="0" checked={this.state.userType === 0} onChange={this.handleTypeChange}/><label htmlFor="consumer" className="checkbox-label">User</label>
+							<input id="agent" type="radio" name="usertype" className="consumer" value="1" checked={this.state.userType === 1} onChange={this.handleTypeChange}/><label htmlFor="agent" className="checkbox-label">Admin</label>
+						</FormGroup>
+						<Button id="submitbtn" type="submit" className="login-btn"><span className="">{btnName}</span></Button>
+					</div>
+					<div className="links-wrap">
+						<div className="label-wrap">
+							<label className={"link-label sign-in-fields " + hideClass} onClick={this.handleForgotPassword}>Forgot Password?</label>
 						</div>
-						<Button id="submitbtn" type="submit" className="login-btn"><span className="">{this.state.selectedOption}</span></Button>
-					</form>
-				 </div>
+						<div className="label-wrap">
+							<input id="login" type="radio" name="tab" className="loginlink" value="false" checked={!this.state.register} onClick={this.handleOptionChange}/>
+							<span className={"checkbox-label sign-up-fields " + hideClass}>Already have an account?</span><label htmlFor="login" className={"link-label sign-up-fields " + hideClass}>Login</label>
+						</div>
+						<div className="label-wrap">
+							<input id="register" type="radio" name="tab" className="registerlink" value="true" checked={this.state.register} onClick={this.handleOptionChange}/>
+							<span className={"checkbox-label sign-in-fields " + hideClass}>Don't have an account?</span><label htmlFor="register" className={"link-label sign-in-fields " + hideClass}>Register</label>
+						</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
