392b6adf3ef9f0c84f16952cc3e7f834d192e29d
MUKTEVI GOPI KRISHNA <muktevigk@handbookinc.com>
2019-05-15 22:08:24 +0530

    Added bootstrap modal

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react';
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import Header from "./layout/Header";
+import Model from "./layout/Model";
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <Header/>
-                <Login/>
+            <div className="container">
+                <Model/>
             </div>
         );
     }
