6f01f76df2d1690ca64ea2dc846884a58ef2e665
VSCG <39382021+VSCG@users.noreply.github.com>
2019-05-15 17:33:12 +0530

    Login formatting and minor fixes

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -4,39 +4,21 @@ import TextField from '../common/TextField';
 import BitField from '../common/BitField';
 
 export default class Login extends Component {
-  constructor(props) {
-	super(props);
-
-	this.state = {
+	state = {
 	  username: "",
-	  email: "",
 	  password: "",
 	  keepSignedIn: false,
-	  register: false,
 	  userType: 0
-	};
-	this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange = event => {
-	  var value = (event.target.type === 'checkbox') ? event.target.checked : event.target.value;
-	  
+	  var value = (event.target.type === 'checkbox') ? event.target.checked : event.target.value;	  
 	  this.setState({[event.target.id]: value});
-  };
-  handleOptionChange = changeEvent => {
-	  this.setState({register: JSON.parse(changeEvent.target.value)});
-  };
-  handleTypeChange = changeEvent => {
-	  this.setState({userType: JSON.parse(changeEvent.target.value)});
-  };
-  
+  };  
   handleSubmit = event => {
 	  event.preventDefault();
   };
-  handleForgotPassword = event => {
-	  event.preventDefault();
-  };
-//
+
   render() {
 	return (
 	<div className="login-wrap">
@@ -47,17 +29,44 @@ export default class Login extends Component {
 			<div className="login-form">
 				<form onSubmit={this.handleSubmit}>
 					<div className="form-wrap">
-						<TextField required autoFocus id="username" type="text" placeholder="Username" value={this.state.username} onChange={this.handleChange}/>
-						<TextField required id="password" type="password" placeholder="Password" pattern = "(?=^.{6,}$).*$" title = "Min 6 characters" value={this.state.password} onChange={this.handleChange}/>
-						<BitField id="keepsignedin" type="checkbox" checked={this.state.keepSignedIn} onChange={this.handleChange} text="Keep Signed In"/>
-						<button id="submitbtn" type="submit" className="login-btn"><span className="">Login</span></button>
+						<TextField 
+							required autoFocus 
+							id="username" 
+							type="text" 
+							placeholder="Username" 
+							value={this.state.username} 
+							onChange={this.handleChange}
+						/>
+						<TextField 
+							required 
+							id="password" 
+							type="password" 
+							placeholder="Password" 
+							pattern = "(?=^.{6,}$).*$" 
+							title = "Min 6 characters" 
+							value={this.state.password} 
+							onChange={this.handleChange}
+						/>
+						<BitField 
+							id="keepsignedin" 
+							type="checkbox" 
+							checked={this.state.keepSignedIn} 
+							onChange={this.handleChange} 
+							text="Keep Signed In"/>
+						<button 
+							id="submitbtn" 
+							type="submit" 
+							className="login-btn">
+							<span className="">Login</span>
+						</button>
 					</div>
 					<div className="links-wrap">
 						<div className="label-wrap">
 							<label className="link-label" onClick={this.handleForgotPassword}>Forgot Password?</label>
 						</div>
 						<div className="label-wrap">
-							<span className="checkbox-label">Don't have an account? </span><label className="link-label">Register</label>
+							<span className="link-label pre">Don't have an account? </span>
+							<label className="link-label">Register</label>
 						</div>
 					</div>
 				</form>
