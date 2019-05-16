8258ef0b9a2748d2ef49b789b588e402f93fdfb4
Gopi Krishna M <fromgopi@yahoo.com>
2019-05-16 12:45:47 +0530

    Revert "Added bootstrap modal"

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import React, {Component} from 'react';
 import Login from "./auth/Login";
 import Register from "./auth/Register";
 import Header from "./layout/Header";
-import Model from "./layout/Model";
 
 class App extends Component {
     render() {
         return (
-            <div className="container">
-                <Model/>
+            <div>
+                <Header/>
+                <Login/>
             </div>
         );
     }
