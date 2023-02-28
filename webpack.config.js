7fc1c162fe13d12aa6e63f0451c2df26bb47ac79
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-02-28 19:49:27 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {    
         path: path.join(__dirname,'build'), 
         filename: 'bundle.js' 
-    },
+    }, 
     mode: process.env.NODE_ENV || 'development',
     resolve: { 
         modules: [path.resolve(__dirname, 'src'), 'node_modules']
