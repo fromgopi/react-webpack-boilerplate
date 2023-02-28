35733cad0302395ad901f0ee13f290f37d7f918c
Gopi Krishna M <gopi.muktevi@gmail.com>
2023-02-28 19:45:20 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
  
- 
+  
 // This is the main webpack file for the project to run.  
 module.exports = {  
     entry: path.join(__dirname,'src','index.js'),
